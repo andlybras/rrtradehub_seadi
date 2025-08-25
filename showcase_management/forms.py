830d1872fcecc8fdf4b1f1307e600713dc707314
Andly <andlybras.desenvolvedor@outlook.com>
2025-08-25 10:10:46 -0400

    Implementa a criação, edição e exclusão de produtos

--- showcase_management/forms.py
+++ showcase_management/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product, ProductImage, NCMSubitem
+from .models import Product, NCMSubitem
 
 class ProductForm(forms.ModelForm):
     ncm_subitem = forms.ModelChoiceField(
@@ -16,11 +16,8 @@ class ProductForm(forms.ModelForm):
             'description': 'Descrição',
             'is_active': 'Manter produto ativo na vitrine',
         }
-
-class ProductImageForm(forms.ModelForm):
-    class Meta:
-        model = ProductImage
-        fields = ['image']
         widgets = {
-            'image': forms.ClearableFileInput(attrs={'multiple': True})
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'description': forms.Textarea(attrs={'class': 'form-input', 'rows': 4}),
+            'is_active': forms.CheckboxInput(attrs={'class': 'h-4 w-4 rounded border-gray-300 text-primary focus:ring-primary'}),
         }
