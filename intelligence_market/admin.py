09f57e5233fb406a8f90297ae2400a009183f0dc
Andly <andlybras.desenvolvedor@outlook.com>
2025-08-26 15:14:05 -0400

    Configura o painel de admin para o app intelligence_market

--- intelligence_market/admin.py
+++ intelligence_market/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import AnalysisPage, Chart
 
-# Register your models here.
+@admin.register(Chart)
+class ChartAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'shortcode_display')
+    readonly_fields = ('slug',)
+    search_fields = ('name',)
+
+    def shortcode_display(self, obj):
+        return obj.get_shortcode()
+    shortcode_display.short_description = "Shortcode (para copiar)"
+
+@admin.register(AnalysisPage)
+class AnalysisPageAdmin(admin.ModelAdmin):
+    list_display = ('card_title', 'is_published', 'updated_at')
+    list_filter = ('is_published',)
+    search_fields = ('title', 'card_title')
