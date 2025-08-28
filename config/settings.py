f7aafcd8505a59562696e97fc0da9c31b891f719
Andly <andlybras.desenvolvedor@outlook.com>
2025-08-28 09:16:49 -0400

    Adiciona configuração STATIC_ROOT para produção

--- config/settings.py
+++ config/settings.py
@@ -103,6 +103,8 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_production')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user_management.CustomUser'
