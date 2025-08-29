0c9affa8577e80fae110a2c6b682f14643e2a664
Andly <andlybras.desenvolvedor@outlook.com>
2025-08-29 14:23:15 -0400

    Insere o LANGUAGES

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from django.utils.translation import gettext_lazy as _
 
 load_dotenv()
 
@@ -94,6 +95,12 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+]
+
+LANGUAGES = [
+    ('pt-br', _('Brazilian Portuguese')),
+    ('en', _('English')),
+    ('es', _('Spanish')),
 ]
 
 LANGUAGE_CODE = 'pt-br'
